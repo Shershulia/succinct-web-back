15c2031a661da9e5de38d0b9541c784238ce5a66
Ivan <krol31072002@gmail.com>
2025-07-15 00:39:51 +0200

    Deploy 6

--- run.py
+++ run.py
@@ -5,9 +5,7 @@ from flask_cors import CORS
 app = create_app()
 
 # Настраиваем CORS (можешь расширить список origin-ов)
-CORS(app, resources={r"/*": {"origins": [
-    "https://succinct-web-front.vercel.app"
-]}})
+CORS(app, resources={r"/*": {"origins": ["https://succinct-web-front.vercel.app"]}})
 
 # Это нужно только для локального запуска
 if __name__ == '__main__':
