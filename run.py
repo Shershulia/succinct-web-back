3d4d00b1da35c88310c8dadd87a95e7cadca417e
Ivan <krol31072002@gmail.com>
2025-07-14 23:15:44 +0200

    Deploy 4

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from app import create_app
 from flask_cors import CORS
 
+# Создаём приложение сразу, на верхнем уровне
 app = create_app()
 
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://yourdomain.com"]}})
+# Настраиваем CORS (можешь расширить список origin-ов)
+CORS(app, resources={r"/*": {"origins": [
+    "https://yourdomain.com"
+]}})
 
+# Это нужно только для локального запуска
 if __name__ == '__main__':
     app.run(debug=True)
